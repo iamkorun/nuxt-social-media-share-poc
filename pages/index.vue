4b097bec4c268d46d807690cd4543863c524b2de
iamkn <thanakorn.jusvw@gmail.com>
2024-10-15 17:17:35 +0700

    fix: image url

--- pages/index.vue
+++ pages/index.vue
@@ -2,11 +2,17 @@
   <img src="/images/cat.png" alt="A cute cat" />
   <div>
     <!-- Facebook Share Button -->
-    <button @click="shareOnFacebook" aria-label="Share on Facebook">Share on Facebook</button>
+    <button @click="shareOnFacebook" aria-label="Share on Facebook">
+      Share on Facebook
+    </button>
     <!-- Twitter Share Button -->
-    <button @click="shareOnTwitter" aria-label="Share on Twitter">Share on Twitter</button>
+    <button @click="shareOnTwitter" aria-label="Share on Twitter">
+      Share on Twitter
+    </button>
     <!-- LinkedIn Share Button -->
-    <button @click="shareOnLinkedIn" aria-label="Share on LinkedIn">Share on LinkedIn</button>
+    <button @click="shareOnLinkedIn" aria-label="Share on LinkedIn">
+      Share on LinkedIn
+    </button>
   </div>
 </template>
 
@@ -14,20 +20,26 @@
 const config = useRuntimeConfig();
 const pageUrl = ref(config.public.WEB_URL);
 const title = 'This is cat title';
-const imageUrl = `${pageUrl}/images/cat.png`;
+const imageUrl = `${pageUrl.value}/images/cat.png`;
 
 const shareOnFacebook = () => {
-  const url = `https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(pageUrl.value)}`;
+  const url = `https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(
+    pageUrl.value
+  )}`;
   window.open(url, '_blank', 'width=600,height=400');
 };
 
 const shareOnTwitter = () => {
-  const url = `https://twitter.com/intent/tweet?url=${encodeURIComponent(pageUrl.value)}&text=${encodeURIComponent(title)}`;
+  const url = `https://twitter.com/intent/tweet?url=${encodeURIComponent(
+    pageUrl.value
+  )}&text=${encodeURIComponent(title)}`;
   window.open(url, '_blank', 'width=600,height=400');
 };
 
 const shareOnLinkedIn = () => {
-  const url = `https://www.linkedin.com/shareArticle?mini=true&url=${encodeURIComponent(pageUrl.value)}`;
+  const url = `https://www.linkedin.com/shareArticle?mini=true&url=${encodeURIComponent(
+    pageUrl.value
+  )}`;
   window.open(url, '_blank', 'width=600,height=400');
 };
 
