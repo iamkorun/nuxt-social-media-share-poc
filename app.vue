2328cc8429e30fdf93cf67a7caa72e833f8e95ba
iamkn <thanakorn.jusvw@gmail.com>
2024-10-15 17:03:24 +0700

    fix: add seo site

--- app.vue
+++ app.vue
@@ -7,11 +7,8 @@
 </template>
 
 <script setup lang="ts">
-
 useSeoMeta({
-  title: 'This is cat title',
-  description: 'A description about the cat',
-  ogDescription: 'A description about the cat',
-  ogTitle: 'This is cat title',
-});
+  titleTemplate: `%s - Cat Site`,
+  ogSiteName: 'Cat Site',
+})
 </script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -14,6 +14,17 @@ export default defineNuxtConfig({
     },
   },
 
+  modules: ['@nuxtjs/seo'],
+
+  site: {
+    // environment variables provided by Netlify
+    url: process.env.NUXT_PUBLIC_WEB_URL || 'http://localhost:3000',
+    name: 'Cat Social Share',
+    indexable: process.env.BRANCH === 'release' || false, // set indexable only on production, not on branch deploys
+    trailingSlash: true,
+    defaultLocale: 'en',
+  },
+
   ssr: false,
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
